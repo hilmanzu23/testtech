b143ac88af0a60ec5d7ec61a98653239d40635ab
hilmanzu23 <hilmanzutech@gmail.com>
2023-04-30 12:05:14 +0700

    feat:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/user/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/core/constant/constant';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,44 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException, UseGuards } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { User } from 'src/user/schemas/user.schema';
 import mongoose from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { comparePassword } from 'src/util/bcrypt';
+import { comparePassword } from 'src/core/util/bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: mongoose.Model<User>,
+    private jwtService: JwtService,
   ) {}
 
+  @UseGuards()
   async login(loginAuthDto: LoginAuthDto) {
     const { email, password } = loginAuthDto;
-    const cekUser = await this.checkAvailableUser(email);
-    if (cekUser.length !== 0) {
-      const hash = await comparePassword(password, cekUser[0].password);
-      if (!hash) throw new NotFoundException('Wrong Email and Password');
-      const resultValue = {
-        id: cekUser[0].id,
-        fullName: cekUser[0].fullName,
-        email: cekUser[0].email,
-        accessToken: 'asdasdasd',
-        refreshToken: 'asdasdasdasd',
-      };
-      return resultValue;
-    } else {
-      throw new NotFoundException('Email Not Registered');
-    }
+    const cekUserAvailable = await this.checkAvailableUser(email);
+    await this.checkPasswordUser(password, cekUserAvailable.password);
+    const payload = {id: cekUserAvailable.id,email: cekUserAvailable.email}
+    const resultValue = {
+      id: cekUserAvailable.id,
+      fullName: cekUserAvailable.fullName,
+      email: cekUserAvailable.email,
+      access_token: await this.jwtService.signAsync(payload),
+      refresh_token: await this.jwtService.signAsync(payload),
+    };
+    return resultValue;
   }
 
   async checkAvailableUser(email: string) {
-    const userDb = await this.userModel.find({ email: email });
+    const userDb = await this.userModel.findOne({ email: email });
+    if (!userDb) throw new NotFoundException('Email Not Registered');
     return userDb;
+  }
+  async checkPasswordUser(password: string, passwordDb: string) {
+    const cekPassword = await comparePassword(password, passwordDb);
+    if (!cekPassword) throw new NotFoundException('Wrong Email and Password');
+    return cekPassword;
   }
 }
