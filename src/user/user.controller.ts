013f53c17e444b4be0cbd85bf24cf1a8297fa56e
hilmanzu23 <hilmanzutech@gmail.com>
2023-04-30 16:05:12 +0700

    feat:

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
+import { AuthGuard } from '../core/util/auth.guard';
 
 @Controller('api')
 export class UserController {
@@ -21,21 +23,25 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get('user')
   findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get('getProfile/:id')
   findOne(@Param('id') id: string): Promise<User> {
     return this.userService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Put('updateProfile/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserSchema } from './schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
